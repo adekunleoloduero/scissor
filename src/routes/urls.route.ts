55f63623de19b71b88c6a3aaacb15f8d5a3abc56
Sheriff Adekunle Tajudeen <dnexteinstein2@gmail.com>
2023-06-14 14:45:39 +0100

    Create url history functionality

--- src/routes/urls.route.ts
+++ src/routes/urls.route.ts
@@ -4,7 +4,8 @@ import {
     shortUrlController, 
     returnLongUrlController, 
     customShortUrlController,
-    urlsAnalyticsController
+    urlsAnalyticsController,
+    urlsHistoryController
 } from '../controllers/url.controller';
 
 import { authenticateUser } from '../middlewares/auth.middleware';
@@ -15,6 +16,7 @@ export default (router: express.Router ): void => {
     router.get('/urls', shortUrlController);
     router.get('/:urlCode', returnLongUrlController);
     router.get('/urls/analytics', authenticateUser, urlsAnalyticsController);
+    router.get('/urls/history', authenticateUser, urlsHistoryController);
     router.post('/urls/customize', authenticateUser, customShortUrlController);
     
 }
