3546d65b611d6ab3e691d4dc432451d9dd2ffffd
Sheriff Adekunle Tajudeen <dnexteinstein2@gmail.com>
2023-07-05 15:51:50 +0100

    Fix: copy functionality to copy short url to the clipboard

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,5 @@
-const analyticsTableRows = document.querySelectorAll('.analytics-row');
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
-
 
 
 hamburger.addEventListener("click", mobileMenu);
@@ -10,30 +8,6 @@ function mobileMenu() {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 }
-// const copyBtn = document.getElementById('copy-short-url');
-// const shortUrlText = document.getElementById('short-url-text');
 
 
 
-// const copyShortUrl = (e) => {
-//     const text = shortUrlText.value;
-//     console.log(text);
-//     navigator.clipboard.writeText(text);
-//     copyBtn.innerText = 'Copied to Clipboard';
-// }
-
-
-
-
-function colorTableRows() {
-    for (let i = 0; i <= analyticsTableRows.length; i++) {
-        if (i % 2 === 0 || i == 0) {
-            analyticsTableRows[i].style.backgroundColor = 'rgba(0, 0, 0, 0.548)';
-        } else {
-            analyticsTableRows[i].style.backgroundColor = 'rgba(0, 0, 0, 0.311)'; 
-        }
-    }
-}
-
-colorTableRows();
-// shortUrlText.addEventListener("click", copyShortUrl);
